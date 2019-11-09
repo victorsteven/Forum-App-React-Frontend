90702cf193aa57db2a8abcb862e8e5f5c5193952
steven victor <chikodi543@gmail.com>
2019-11-09 13:26:52 +0100

    Debug

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,9 +5,10 @@ import Navigation from './Navigation'
 
   const Dashboard = () => {
     return (
-      <div>
-        <Navigation />
-        <Posts /> 
+      <div className="mt-4" style={{ textAlign: "center"}}>
+        {/* <Navigation />
+        <Posts />  */}
+        <h1>We will be right back in 30 min</h1>
       </div>
     )
   }
